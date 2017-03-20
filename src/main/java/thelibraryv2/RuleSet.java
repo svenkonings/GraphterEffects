e9a0123eae260b04633e4ee55b58e06db0a2b3a2
Charrey <p.vanleeuwen@student.utwente.nl>
2017-03-20 12:17:24 +0100

    Interface "Completed Rules" documented

--- src/main/java/thelibraryv2/RuleSet.java
+++ src/main/java/thelibraryv2/RuleSet.java
@@ -8,8 +8,17 @@ import java.util.Map;
  */
 public class RuleSet {
 
+    /**
+     * Container of the completed rules
+     */
     private Map<VisElem, Map<String, String>> completedrules = new HashMap<>();
 
+    /**
+     * Set a completed rule
+     * @param vis Visualisation element of which parameters are set
+     * @param key Key of the parameter
+     * @param value Value of the parameter
+     */
     public void set(VisElem vis, String key, String value) {
         if (!completedrules.containsKey(vis)) {
             completedrules.put(vis, new HashMap<>());
