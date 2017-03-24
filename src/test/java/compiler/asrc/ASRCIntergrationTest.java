6f91ed131c4a01c5994a9d74dc13a9eceba95f71
HansvdLaan <j.h.vanderlaan@student.utwente.nl>
2017-03-24 10:20:09 +0100

    ASRCIntergrationTest fails. Commit for Pim so he can fix it.

--- src/test/java/compiler/asrc/ASRCIntergrationTest.java
+++ src/test/java/compiler/asrc/ASRCIntergrationTest.java
@@ -4,6 +4,7 @@ import compiler.graphloader.Importer;
 import org.graphstream.graph.Graph;
 import org.junit.Test;
 import utils.FileUtils;
+import utils.Printer;
 import za.co.wstoop.jatalog.Jatalog;
 
 import static compiler.asrc.GraphRuleTests.generateGraphJatalog;
@@ -14,8 +15,7 @@ public final class ASRCIntergrationTest {
     @Test
     public void intergrationTest1() throws Exception {
         Graph graph = Importer.fromFile(FileUtils.fromResources("asrc_testgraphs/graph1.dot"));
-
-
+        Printer.pprint(graph);
 
         Jatalog jatalog = generateGraphJatalog(graph);
         graphTest(jatalog, graph);
