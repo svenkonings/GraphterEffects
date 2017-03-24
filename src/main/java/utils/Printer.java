73df438a49c81f36920cbd0d8931fa1d22a3f8ca
Charrey <p.vanleeuwen@student.utwente.nl>
2017-03-24 10:49:46 +0100

    Fixed double quotes in pprint

--- src/main/java/utils/Printer.java
+++ src/main/java/utils/Printer.java
@@ -30,22 +30,31 @@ public final class Printer {
         List<Element> nodeset = new LinkedList<>(g.getNodeSet());
         Collections.sort(nodeset, (o1, o2) -> StringUtils.compareStrings(o1.getId(), o2.getId()));
         builder.append("NODES\n");
-        addfromlist(builder, nodeset);
+        addfromlist(builder, nodeset, false, false);
         List<Element> edgeset = new LinkedList<>(g.getEdgeSet());
         Collections.sort(edgeset, (o1, o2) -> StringUtils.compareStrings(o1.getId(), o2.getId()));
         builder.append("EDGES\n");
-        addfromlist(builder, edgeset);
+        addfromlist(builder, edgeset, false ,false);
         System.out.println(builder.toString());
     }
 
 
-    private static void addfromlist(StringBuilder builder, List<Element> list) {
+    private static void addfromlist(StringBuilder builder, List<Element> list, boolean keyquotes, boolean valuequotes) {
+        String keyq = "";
+        String valq = "";
+        if (keyquotes) {
+            keyq = "\"";
+        }
+        if (valuequotes) {
+            valq = "\"";
+        }
+
         for (Element e : list) {
             builder.append("\t").append(e);
             builder.append("\t{");
             String attrstring = "";
             for (String key : e.getAttributeKeySet()) {
-                attrstring += "\"" + key + "\":\"" + e.getAttribute(key) + "\",";
+                attrstring += keyq + key + keyq + ":" + valq + e.getAttribute(key) + valq + ",";
             }
             if (attrstring.endsWith(",")) {
                 attrstring = attrstring.substring(0, attrstring.length()-1);
