f42aab4ff09fdaade3eeb14e619ea428fb70bb8f
Ron van Bree <ronvbree@gmail.com>
2017-03-27 14:37:49 +0200

    Updated grammar and test

--- test/LiteralTest.java
+++ test/LiteralTest.java
@@ -9,11 +9,22 @@ import java.util.List;
  */
 public class LiteralTest extends GrammarTest {
 
+    private static final List<String> EQ_EXPR = Arrays.asList
+            (
+                    "%s %s %s"
+            );
+
     public static final List<String> VALID_SAMPLES = new ArrayList<>();
     static {
         VALID_SAMPLES.addAll(AtomTest.VALID_SAMPLES);
         VALID_SAMPLES.addAll(MultiAtomTest.VALID_SAMPLES);
-//        VALID_SAMPLES.addAll(BoolExprTest.VALID_SAMPLES);
+        // Add eq expr
+        for (String sample : NumExprTest.VALID_SAMPLES) {
+            VALID_SAMPLES.add(String.format(EQ_EXPR.get(0), sample, VALID_EQ_OP_SAMPLES.get(0), sample));
+        }
+        for (String sample : VALID_EQ_OP_SAMPLES) {
+            VALID_SAMPLES.add(String.format(EQ_EXPR.get(0), NumExprTest.VALID_SAMPLES.get(0), sample, NumExprTest.VALID_SAMPLES.get(0)));
+        }
     }
 
     public static final List<String> INVALID_SAMPLES = Arrays.asList
@@ -23,7 +34,6 @@ public class LiteralTest extends GrammarTest {
                     "test",
                     "_(A)"
             );
-
 
     @Override
     protected List<String> getValidSamples() {
