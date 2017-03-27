b114e7ef77ef531a42f8df48bd9f622be4a45dac
Charrey <p.vanleeuwen@student.utwente.nl>
2017-03-27 15:49:11 +0200

    GraphStreamImporter documented

--- src/main/java/compiler/exceptions/NoSolutionException.java
+++
@@ -1,9 +0,0 @@
-package compiler.exceptions;
-
-public class NoSolutionException extends Exception {
-
-    @Override
-    public String getMessage() {
-        return "No element that fits these parameters could be found.";
-    }
-}
--- src/main/java/compiler/graphloader/GraphStreamImporter.java
+++ src/main/java/compiler/graphloader/GraphStreamImporter.java
@@ -11,14 +11,31 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Class used to import graphs from formats natively supported by GraphStream.
+ */
 final class GraphStreamImporter {
 
+    /**
+     * List of file extensions accepted by this importer.
+     */
     private static final List<String> acceptslist = Arrays.asList("dgs", "dot", "gml", "tlp", "net", "graphml", "net");
 
+    /**
+     * Returns whether an extension is accepted by this importer.
+     * @param ext File extension to verify.
+     * @return <tt>true</tt> if the file extension is accepted.
+     */
     static boolean acceptsExtension(String ext) {
         return acceptslist.contains(ext.toLowerCase());
     }
 
+    /**
+     * Reads a file in some graph format into a GraphStream graph Object.
+     * @param file File to read into a GraphsStream Graph Object.
+     * @return A GraphStream Graph Object containing the graph represented in the file.
+     * @throws IOException Thrown when the file could not be read.
+     */
     static Graph read(File file) throws IOException {
         MultiGraph g = new MultiGraph(file.getName());
         FileSource fs = FileSourceFactory.sourceFor(file.getAbsolutePath());
@@ -28,6 +45,10 @@ final class GraphStreamImporter {
         return g;
     }
 
+    /**
+     * Returns an iterator iterating over all accepted extensions.
+     * @return An iterator iterating over all accepted extensions.
+     */
     public static Iterator<String> accepted() {
         return acceptslist.iterator();
     }
