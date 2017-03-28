9efb972843888cf187ca15527d2014af9f5143c8
meteoorkip <svenkonings@live.nl>
2017-03-28 23:52:17 +0200

    Fluent TuProlog API

---
+++ src/experimental/java/prolog/TuProlog.java
@@ -0,0 +1,95 @@
+package prolog;
+
+import alice.tuprolog.Double;
+import alice.tuprolog.Float;
+import alice.tuprolog.*;
+import alice.tuprolog.Long;
+import alice.tuprolog.Number;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+
+public class TuProlog {
+    // More generic version, parses the given String
+    public static Term term(String term) {
+        return Term.createTerm(term);
+    }
+
+    // More generic version, parses the given String
+    public static Number number(String number) {
+        return Number.createNumber(number);
+    }
+
+    public static Int intVal(int value) {
+        return new Int(value);
+    }
+
+    public static Long longVal(long value) {
+        return new Long(value);
+    }
+
+    public static Float floatVal(float value) {
+        return new Float(value);
+    }
+
+    public static Double doubleVal(double value) {
+        return new Double(value);
+    }
+
+    public static Var var() {
+        return new Var();
+    }
+
+    public static Var var(String name) {
+        return new Var(name);
+    }
+
+    public static Struct atom() {
+        return new Struct();
+    }
+
+    public static Struct atom(String name) {
+        return new Struct(name);
+    }
+
+    public static Struct list(Term... terms) {
+        return new Struct(terms);
+    }
+
+    public static Struct compound(String name, Term... terms) {
+        return new Struct(name, terms);
+    }
+
+    public static Struct fact(Struct fact) {
+        return clause(fact, atom("true"));
+    }
+
+    public static Struct clause(Struct head, Struct body) {
+        return new Struct(":-", head, body);
+    }
+
+    public static Struct and(Term... terms) {
+        return concatTerms(",", terms);
+    }
+
+    public static Struct or(Term... terms) {
+        return concatTerms(";", terms);
+    }
+
+    public static Struct concatTerms(String name, Term... terms) {
+        Deque<Term> deque = new ArrayDeque<>(Arrays.asList(terms));
+        while (deque.size() < 2) {
+            deque.addLast(atom());
+        }
+        Term term2 = deque.removeLast();
+        Term term1 = deque.removeLast();
+        Struct struct = compound(name, term1, term2);
+        while (!deque.isEmpty()) {
+            Term term = deque.removeLast();
+            struct = compound(name, term, struct);
+        }
+        return struct;
+    }
+}
