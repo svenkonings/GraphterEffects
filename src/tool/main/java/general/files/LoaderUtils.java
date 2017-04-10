69bbccceb107c8cd96f3e75756d016f485587e46
HansvdLaan <j.h.vanderlaan@student.utwente.nl>
2017-04-10 09:59:24 +0200

    Another small fix

--- src/tool/main/java/general/files/LoaderUtils.java
+++ src/tool/main/java/general/files/LoaderUtils.java
@@ -26,7 +26,7 @@ public class LoaderUtils {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select GraafVis Script");
         String path = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
-        path += "/tests";
+        //path += "/tests";
         fileChooser.setInitialDirectory(new File(path));
 
         fileChooser.getExtensionFilters().add(visFilesFilter);
