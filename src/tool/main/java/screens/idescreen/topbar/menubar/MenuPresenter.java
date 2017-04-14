ee6a6f216e68e583b0fd64d94fcd922157689be9
HansvdLaan <j.h.vanderlaan@student.utwente.nl>
2017-04-14 14:27:59 +0200

    Created an about popup

--- src/tool/main/java/screens/idescreen/topbar/menubar/MenuPresenter.java
+++ src/tool/main/java/screens/idescreen/topbar/menubar/MenuPresenter.java
@@ -1,18 +1,24 @@
 package screens.idescreen.topbar.menubar;
 
 import general.ViewModel;
+import general.compiler.CompilerRunnable;
 import general.files.DocumentModel;
 import general.files.IOManager;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import screens.idescreen.about.AboutView;
+import screens.idescreen.simpleviewer.SimpleViewerPresenter;
+import screens.idescreen.simpleviewer.SimpleViewerView;
+import utils.FileUtils;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -20,6 +26,7 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -51,7 +58,7 @@ public class MenuPresenter implements Initializable {
         menuBar.prefWidthProperty().bind(borderPane.prefWidthProperty());
     }
 
-    public void exitButtonPressed(ActionEvent actionEvent) {
+    public void exitMenuItemPressed(ActionEvent actionEvent) {
         if (!DocumentModel.getInstance().graafvisChangesSaved()){
             if(!IOManager.showGraafvisScriptSaveDialog(DocumentModel.getInstance().getGraafVisFilePath(), DocumentModel.getInstance().graafVisCode)){
                 actionEvent.consume();
@@ -70,7 +77,7 @@ public class MenuPresenter implements Initializable {
         System.exit(0);
     }
 
-    public void loadScriptButtonPressed(ActionEvent actionEvent) {
+    public void loadScriptMenuItemPressed(ActionEvent actionEvent) {
         if (!DocumentModel.getInstance().graafvisChangesSaved()){
             if(IOManager.showGraafvisScriptSaveDialog(DocumentModel.getInstance().getGraafVisFilePath(), DocumentModel.getInstance().graafVisCode)){
                 IOManager.showLoadScriptPopup();
@@ -82,7 +89,7 @@ public class MenuPresenter implements Initializable {
 
     }
 
-    public void newScriptButtonPressed(ActionEvent actionEvent) {
+    public void newScriptMenuItemPressed(ActionEvent actionEvent) {
         if (!DocumentModel.getInstance().graafvisChangesSaved()){
             if(IOManager.showGraafvisScriptSaveDialog(DocumentModel.getInstance().getGraafVisFilePath(), DocumentModel.getInstance().graafVisCode)){
                 DocumentModel.getInstance().newGraafVisFile();
@@ -93,7 +100,7 @@ public class MenuPresenter implements Initializable {
         DocumentModel.getInstance().newGraafVisFile();
     }
 
-    public void saveButtonPressed(ActionEvent actionEvent) {
+    public void saveMenuItemPressed(ActionEvent actionEvent) {
         //IOManager.showSaveScriptPopup(Model.getInstance().getCodePaneTextArea().getText());
         TabPane tabPane = (TabPane) (((BorderPane) (viewModel.getMainView())).getCenter());
         Tab viewerTab = tabPane.getSelectionModel().getSelectedItem();
@@ -115,14 +122,14 @@ public class MenuPresenter implements Initializable {
         }
     }
 
-    public void propertiesButtonPressed(ActionEvent actionEvent) {
+    public void propertiesMenuItemPressed(ActionEvent actionEvent) {
     }
 
-    public void loadGraphButtonPressed(ActionEvent actionEvent) {
+    public void loadGraphMenuItemPressed(ActionEvent actionEvent) {
         IOManager.showLoadGraphsPopup(false);
     }
 
-    public void saveAsButtonPressed(ActionEvent actionEvent) {
+    public void saveAsMenuItemPressed(ActionEvent actionEvent) {
         TabPane tabPane = (TabPane) (((BorderPane) (viewModel.getMainView())).getCenter());
         Tab viewerTab = tabPane.getSelectionModel().getSelectedItem();
         String viewerTabID = viewerTab.getText();
@@ -134,5 +141,55 @@ public class MenuPresenter implements Initializable {
         } else {
             IOManager.showSaveSVGPopup(DocumentModel.getInstance().getGeneratedSVG(viewerTabID));
         }
+    }
+
+    public void graphAsImageMenuItemPressed(ActionEvent actionEvent) {
+        BorderPane centralBorder = ((BorderPane) (((AnchorPane) borderPane.getParent()).getParent().getParent()).getParent());
+        SplitPane topBar = (SplitPane) centralBorder.getTop();
+        ButtonBar buttonBar = (ButtonBar) ((SplitPane) ((AnchorPane) topBar.getItems().get(1)).getChildren().get(0)).getItems().get(1);
+        ComboBox comboBox = (ComboBox) buttonBar.getButtons().get(0);
+        String selectedGraphName = comboBox.getSelectionModel().getSelectedItem().toString();
+        //String selectedGraphName = graphComboBox.getSelectionModel().getSelectedItem().toString();
+        Path selectedGraphPath = DocumentModel.getInstance().getGraphPathMap().get(selectedGraphName);
+        CompilerRunnable compilerRunnable = new CompilerRunnable(Paths.get("defaultvisualization.vis"), selectedGraphPath);
+        new Thread(compilerRunnable).start();
+    }
+
+    public void graphAsTextMenuItemPressed(ActionEvent actionEvent) {
+        BorderPane centralBorder = ((BorderPane) (((AnchorPane) borderPane.getParent()).getParent().getParent()).getParent());
+        SplitPane topBar = (SplitPane) centralBorder.getTop();
+        ButtonBar buttonBar = (ButtonBar) ((SplitPane) ((AnchorPane) topBar.getItems().get(1)).getChildren().get(0)).getItems().get(1);
+        ComboBox comboBox = (ComboBox) buttonBar.getButtons().get(0);
+        String selectedGraphName = comboBox.getSelectionModel().getSelectedItem().toString();
+
+        Path selectedGraphPath = DocumentModel.getInstance().getGraphPathMap().get(selectedGraphName);
+        String graphAsString = "";
+        try {
+            graphAsString = FileUtils.readFromFile(selectedGraphPath.toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        TabPane centralTabPane = (TabPane) centralBorder.getCenter();
+        SimpleViewerView simpleViewerView = new SimpleViewerView();
+        SimpleViewerPresenter simpleViewerPresenter = (SimpleViewerPresenter) simpleViewerView.getPresenter();
+        simpleViewerPresenter.loadContent(selectedGraphName, graphAsString);
+        centralTabPane.getTabs().add(new Tab(selectedGraphName, simpleViewerView.getView()));
+    }
+
+    public void undoChangesMenutemPressed(ActionEvent actionEvent) {
+        Path graafvisScriptPath = DocumentModel.getInstance().getGraafVisFilePath();
+        if (!DocumentModel.getInstance().graafvisChangesSaved()) {
+            if (IOManager.showGraafvisScriptSaveDialog(graafvisScriptPath, DocumentModel.getInstance().graafVisCode)) {
+                DocumentModel.getInstance().loadGraafVisFile(graafvisScriptPath);
+            }
+        }
+    }
+
+    public void helpMenuItemPressed(ActionEvent actionEvent) {
+        AboutView aboutView = new AboutView();
+        Scene scene = new Scene(aboutView.getView());
+        Stage aboutDialog = new Stage();
+        aboutDialog.setScene(scene);
+        aboutDialog.show();
     }
 }
