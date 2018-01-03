4fa1ab7535f9bd7950fbd2160326f63b380eba4a
Pim van Leeuwen <poesdomino@gmail.com>
2018-01-03 14:44:48 +0100

    Possibly fixed Travis (again)

--- GraphterEffectsLibrary/src/test/java/com/github/meteoorkip/cli/CommandLineTest.java
+++ GraphterEffectsLibrary/src/test/java/com/github/meteoorkip/cli/CommandLineTest.java
@@ -1,16 +1,12 @@
 package com.github.meteoorkip.cli;
 
-import alice.tuprolog.InvalidTheoryException;
 import com.github.meteoorkip.GraphterEffects;
-import com.github.meteoorkip.graafvis.GraafvisCompiler;
-import com.github.meteoorkip.utils.*;
+import com.github.meteoorkip.utils.FileUtils;
+import com.github.meteoorkip.utils.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
@@ -33,24 +29,24 @@ public class CommandLineTest {
 
     @Test
     public void testNumberOfArguments() throws Exception {
-        assertEquals("Error: Expected 3 arguments, received 0. Type --help for help.", getString(new String[]{}));
-        assertEquals("Error: Expected 3 arguments, received 1. Type --help for help.", getString(new String[]{"a"}));
-        assertEquals("Error: Expected 3 arguments, received 4. Type --help for help.", getString(new String[]{graphLoc, visLoc, svgLoc, "Hi!"}));
-        assertEquals("Error: Expected 2 arguments, received 3. Type --help for help.", getString(new String[]{graphLoc, visLoc, svgLoc, "-n"}));
-        assertEquals("", getString(new String[]{graphLoc, visLoc, svgLoc}));
-        assertEquals("Error: If help flag is used, no arguments are expected. Type --help for help.", getString(new String[]{graphLoc, visLoc, svgLoc, "--help"}));
+        assertEqualsIgnoreNewlines("Error: Expected 3 arguments, received 0. Type --help for help.", getString(new String[]{}));
+        assertEqualsIgnoreNewlines("Error: Expected 3 arguments, received 1. Type --help for help.", getString(new String[]{"a"}));
+        assertEqualsIgnoreNewlines("Error: Expected 3 arguments, received 4. Type --help for help.", getString(new String[]{graphLoc, visLoc, svgLoc, "Hi!"}));
+        assertEqualsIgnoreNewlines("Error: Expected 2 arguments, received 3. Type --help for help.", getString(new String[]{graphLoc, visLoc, svgLoc, "-n"}));
+        assertEqualsIgnoreNewlines("", getString(new String[]{graphLoc, visLoc, svgLoc}));
+        assertEqualsIgnoreNewlines("Error: If help flag is used, no arguments are expected. Type --help for help.", getString(new String[]{graphLoc, visLoc, svgLoc, "--help"}));
     }
 
     @Test
     public void testHelp() throws Exception {
-        assertEquals(GraphterEffects.HELPSTRING, getString(new String[]{"--help"}));
-        assertEquals(GraphterEffects.HELPSTRING, getString(new String[]{"-h"}));
+        assertEqualsIgnoreNewlines(GraphterEffects.HELPSTRING, getString(new String[]{"--help"}));
+        assertEqualsIgnoreNewlines(GraphterEffects.HELPSTRING, getString(new String[]{"-h"}));
     }
 
     @Test
     public void testVersion() throws Exception {
-        assertEquals(GraphterEffects.VERSIONSTRING, getString(new String[]{"--version"}));
-        assertEquals(GraphterEffects.VERSIONSTRING, getString(new String[]{"-v"}));
+        assertEqualsIgnoreNewlines(GraphterEffects.VERSIONSTRING, getString(new String[]{"--version"}));
+        assertEqualsIgnoreNewlines(GraphterEffects.VERSIONSTRING, getString(new String[]{"-v"}));
     }
 
     @Test
@@ -63,19 +59,19 @@ public class CommandLineTest {
 
     @Test
     public void testIncompatibleFlags() throws Exception {
-        assertEquals("Error: If help flag is used, no other flags may be used. Type --help for help.", getString(new String[]{"-vh"}));
+        assertEqualsIgnoreNewlines("Error: If help flag is used, no other flags may be used. Type --help for help.", getString(new String[]{"-vh"}));
     }
 
     @Test
     public void testNoGraph() throws Exception {
-        assertEquals("", getString(new String[]{"-n", visLoc, svgLoc}));
-        assertEquals("", getString(new String[]{visLoc, svgLoc, "-n"}));
-        assertEquals("", getString(new String[]{visLoc, svgLoc, "--nograph"}));
+        assertEqualsIgnoreNewlines("", getString(new String[]{"-n", visLoc, svgLoc}));
+        assertEqualsIgnoreNewlines("", getString(new String[]{visLoc, svgLoc, "-n"}));
+        assertEqualsIgnoreNewlines("", getString(new String[]{visLoc, svgLoc, "--nograph"}));
     }
 
     @Test
     public void testPrintResult() throws Exception {
-        assertEquals(("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+        assertEqualsIgnoreNewlines("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                 "\n" +
                 "<svg xmlns=\"http://www.w3.org/2000/svg\" style=\"max-height:100%\" viewBox=\"0 0 43 43\">\n" +
                 "  <line x1=\"38\" y1=\"16\" x2=\"27\" y2=\"5\" fill=\"white\" stroke=\"black\"/>\n" +
@@ -85,9 +81,12 @@ public class CommandLineTest {
                 "  <ellipse rx=\"5\" ry=\"5\" cx=\"27\" cy=\"5\" fill=\"white\" stroke=\"black\"/>\n" +
                 "  <ellipse rx=\"5\" ry=\"5\" cx=\"5\" cy=\"38\" fill=\"white\" stroke=\"black\"/>\n" +
                 "  <ellipse rx=\"5\" ry=\"5\" cx=\"16\" cy=\"27\" fill=\"white\" stroke=\"black\"/>\n" +
-                "</svg>\n").replaceAll("[\r\n]",""), getString(new String[]{graphLoc, visLoc, "-p"}).replaceAll("[\r\n]",""));
+                "</svg>\n", getString(new String[]{graphLoc, visLoc, "-p"}));
     }
 
+    private static void assertEqualsIgnoreNewlines(String expected, String actual) {
+        assertEquals(expected.replaceAll("[\r\n]",""), actual.replaceAll("[\r\n]",""));
+    }
 
 
     private String getString(String[] args) throws Exception {
